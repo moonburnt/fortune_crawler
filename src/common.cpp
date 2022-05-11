f281b5f094367d50ab55be420a90cc5e168d6c55
moonburnt <moonburnt@disroot.org>
2022-05-11 15:41:39 +0300

    Update engine to latest
    - Update engine to latest.
    - Adjust GuiBuilder to comply with provided changes.

--- src/common.cpp
+++ src/common.cpp
@@ -9,35 +9,57 @@ GuiBuilder::GuiBuilder(App* app)
 
 Button* GuiBuilder::make_close_button() {
     return new Button(
-        app->assets.sprites["cross_default"],
-        app->assets.sprites["cross_hover"],
-        app->assets.sprites["cross_pressed"],
-        app->assets.sounds["button_hover"],
-        app->assets.sounds["button_clicked"],
+        {
+            {ButtonStates::idle, app->assets.sprites["cross_default"]},
+            {ButtonStates::hover, app->assets.sprites["cross_hover"]},
+            {ButtonStates::pressed, app->assets.sprites["cross_pressed"]},
+            {ButtonStates::clicked, app->assets.sprites["cross_default"]},
+            {ButtonStates::disabled, app->assets.sprites["cross_default"]}
+        },
+        {
+            {ButtonStates::hover, app->assets.sounds["button_hover"]},
+            {ButtonStates::clicked, app->assets.sounds["button_clicked"]}
+        },
         Rectangle{0, 0, 64, 64});
 }
 
 Button* GuiBuilder::make_text_button(std::string txt) {
     return new Button(
         txt,
-        app->assets.sprites["button_default"],
-        app->assets.sprites["button_hover"],
-        app->assets.sprites["button_pressed"],
-        app->assets.sounds["button_hover"],
-        app->assets.sounds["button_clicked"],
+        {
+            {ButtonStates::idle, app->assets.sprites["button_default"]},
+            {ButtonStates::hover, app->assets.sprites["button_hover"]},
+            {ButtonStates::pressed, app->assets.sprites["button_pressed"]},
+            {ButtonStates::clicked, app->assets.sprites["button_default"]},
+            {ButtonStates::disabled, app->assets.sprites["button_default"]}
+        },
+        {
+            {ButtonStates::hover, app->assets.sounds["button_hover"]},
+            {ButtonStates::clicked, app->assets.sounds["button_clicked"]}
+        },
         Rectangle{0, 0, 256, 64});
 }
 
 Checkbox* GuiBuilder::make_checkbox(bool default_state) {
     return new Checkbox(
-        app->assets.sprites["toggle_on_default"],
-        app->assets.sprites["toggle_on_hover"],
-        app->assets.sprites["toggle_on_pressed"],
-        app->assets.sprites["toggle_off_default"],
-        app->assets.sprites["toggle_off_hover"],
-        app->assets.sprites["toggle_off_pressed"],
-        app->assets.sounds["button_hover"],
-        app->assets.sounds["button_clicked"],
+        {
+            {ButtonStates::idle, app->assets.sprites["toggle_on_default"]},
+            {ButtonStates::hover, app->assets.sprites["toggle_on_hover"]},
+            {ButtonStates::pressed, app->assets.sprites["toggle_on_pressed"]},
+            {ButtonStates::clicked, app->assets.sprites["toggle_on_default"]},
+            {ButtonStates::disabled, app->assets.sprites["toggle_on_default"]}
+        },
+        {
+            {ButtonStates::idle, app->assets.sprites["toggle_off_default"]},
+            {ButtonStates::hover, app->assets.sprites["toggle_off_hover"]},
+            {ButtonStates::pressed, app->assets.sprites["toggle_off_pressed"]},
+            {ButtonStates::clicked, app->assets.sprites["toggle_off_default"]},
+            {ButtonStates::disabled, app->assets.sprites["toggle_off_default"]}
+        },
+        {
+            {ButtonStates::hover, app->assets.sounds["button_hover"]},
+            {ButtonStates::clicked, app->assets.sounds["button_clicked"]}
+        },
         Rectangle{0, 0, 32, 32},
         default_state);
 }
